3c71b587692b05f4a8be38720f8c770d0c17e284
Kirill Dzhunkovskiy <dzhunkofffski@gmail.com>
2023-12-15 19:54:40 +0300

    asdasd

--- src/loss/rawnet2_loss.py
+++ src/loss/rawnet2_loss.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 class RawNet2Loss(nn.Module):
     def __init__(self) -> None:
         super().__init__()
-        self.cross_entropy = nn.CrossEntropyLoss(weight=torch.tensor([0.5, 0.5]))
+        self.cross_entropy = nn.CrossEntropyLoss(weight=torch.tensor([0.9, 0.1]))
     
     def forward(self, logits, label, **batch):
         return self.cross_entropy(logits, label)
